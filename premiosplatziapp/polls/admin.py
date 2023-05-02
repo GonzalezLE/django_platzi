370574657d99937548082bf1fecf852379a79bf8
GonzalezLE <gonzalez-3216@hotmail.com>
2023-05-01 23:28:51 -0600

    usando detail funtion

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question,Choice
+
+
+# class ChoiceAdmin(admin.ModelAdmin):
+#     readonly_fields = ['vote'] # campo de solo lectura
+
+
+admin.site.register([
+    Question,
+    Choice
+])
+   
+   
+# admin.site.register(apps.all_models.get('polls').values())
